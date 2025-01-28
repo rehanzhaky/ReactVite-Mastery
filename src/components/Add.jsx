77503659d90fd47aaaf45eea7597bdf436f6bae9
rehanzhaky <hannn9491@gmail.com>
2025-01-28 22:49:11 +0700

    chlg: first  component

---
+++ src/components/Add.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Add = () => {
+  return <div>This is Add Page ➕</div>;
+};
+
+export default Add;
---
+++ src/components/Greet.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Greet = () => {
+  return (
+    <div>
+      <h1>This is Greeting Page 👋</h1>
+    </div>
+  );
+};
+
+export default Greet;
