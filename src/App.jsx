0f8eb121446a04649e5f77613720d26b88943313
rehanzhaky <hannn9491@gmail.com>
2025-04-22 09:59:19 +0700

    mod: React Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Props from './components/Props'
 
 const App = () => {
   return (
     <>
         <Navbar />
         <Hero />
+        <Props name="Raihan" message="Kiw, im " />
     </>
   )
 }
---
+++ src/components/Props.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Props = (props) => {
+  return (
+    <>
+      <h1>
+        {props.message} {props.name}
+      </h1>
+    </>
+  );
+};
+
+export default Props;
