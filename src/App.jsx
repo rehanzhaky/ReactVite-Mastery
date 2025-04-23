736d04b894de3551db267297425fb734b2cf214c
rehanzhaky <hannn9491@gmail.com>
2025-04-23 12:51:29 +0700

    mod: Passing Object

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import Props from "./belajar/Props";
 import DestructuringProps from "./belajar/DestructuringProps";
 import ImmutabilityOfProps from "./belajar/ImmutabilityOfProps";
 import PassingArray from "./belajar/PassingArray";
+import PassingObject from "./belajar/PassingObject";
 
 const App = () => {
   // List of Arrays
   const seatNumber = [1, 2, 3];
+
+  // Object
+  const book = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    famousQuotes:
+      "There are no good or bad habits, There are only effective habits",
+  };
   return (
     <>
       <Navbar />
@@ -20,7 +29,9 @@ const App = () => {
       {/* Props bersifat immutable, tidak dapat di reassigned */}
       <ImmutabilityOfProps learn="React" with="codestoic" />
       {/* Mencoba untuk Passing Array */}
-      <PassingArray busName="Jaya Abadi Bus" seatNumber={seatNumber}/>
+      <PassingArray busName="Jaya Abadi Bus" seatNumber={seatNumber} />
+      {/* Mencoba untuk Passing Object */}
+      <PassingObject favThing={book} />
     </>
   );
 };
--- src/belajar/PassingObject.jsx
+++ src/belajar/PassingObject.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const PassingObject = () => {
+const PassingObject = ({ favThing }) => {
   return (
     <div>
-      
+      <h1>
+        I really love to read the {favThing.title}, the best book in the world
+        from 
+        {favThing.author}, and i remember one quote from that book is
+        {favThing.famousQuotes}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default PassingObject
+export default PassingObject;
